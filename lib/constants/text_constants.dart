5ee68f8447e58b209cc493ed819e8b525af3041b
Velma Kimani <stanvelma16@gmail.comm>
2022-03-17 09:24:44 -1200

    String constants

--- lib/constants/text_constants.dart
+++ lib/constants/text_constants.dart
@@ -3,4 +3,33 @@ class Constants {
   static String firstPageHeader = "What do you want to learn today?";
   static String firstPageCardTitle = "Computer Science";
   static String firstPageCardSubtitle = "SCIENCE";
+
+  static String secondPageGreeting = "Hi,";
+  static String secondPageInfo =
+      "Select the additional subjects you would like to make part of your course list";
+  static String secondPageTotal = "4total";
+  static String firstOption = "Combined Science";
+  static String secondOption = "Commerce";
+  static String thirdOption = "Economics";
+  static String fourthOption = "Heritage Studies";
+  static String nextPage = "CONTINUE";
+
+  static String subscriptionCardTitle = "All-in-one-subscription";
+  static String subscriptionCardSubtitle =
+      "Purchase the all-in-one subscription for only ZW1,299.00 and gain access to all the subjects on Kytte for a whole month";
+  static String subscriptionCardButton = "BUY NOW";
+
+  static String paymentHistoryTitle = "Payment history";
+  static String paymentHistorySubtitle =
+      "See payments you have made and also fix errors";
+
+  static String biology = "Biology";
+  static String biologyButton = "PAY NOW";
+  static String biologyTrailingTitle = "Trial expires 15Mar";
+  static String biologyTrailingSubtitle = "zw499.00";
+
+  static String computerScience = "Computer science";
+  static String computerScienceButton = "PAY NOW";
+  static String computerScienceTrailingTitle = "Trial expires 15Mar";
+  static String computerScienceTrailingSubtitle = "zw499.00";
 }
